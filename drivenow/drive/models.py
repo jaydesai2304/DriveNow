e3228332f7ca180a6cde909364b2bb534d7ecb6d
harsh230806 <harsh@nexalussoft.com>
2024-08-27 13:16:09 +0530

    setup register and login

--- drivenow/drive/models.py
+++ drivenow/drive/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    username = models.CharField(max_length=150)
+    fullname = models.CharField(max_length=150, default="")
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
+    password = models.CharField(max_length=8)
+    confirmpassword = models.CharField(max_length=8)
+
+    def __str__(self):
+        return f"{self.username}"
--- drivenow/drive/views.py
+++ drivenow/drive/views.py
@@ -1,3 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.http.request import HttpRequest
+from rest_framework import generics
+from rest_framework.request import Request
+from .serializers import (
+    PersonSerializer,
+    Loginserializer
+)
+from rest_framework.renderers import TemplateHTMLRenderer
+from typing import Union
 
-# Create your views here.
+class RegisterView(generics.CreateAPIView):
+    serializer_class = PersonSerializer
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = "register.html"
+
+    def get(self, request: Union[Request, HttpRequest]) -> Union[render, redirect]:
+        if "username" in request.session:
+            return redirect("home")
+        serializer = PersonSerializer()
+        return render(request, self.template_name, {"serializer": serializer})
+
+    def post(self, request: Union[Request, HttpRequest]) -> Union[redirect, render]:
+        serializer = PersonSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return redirect("login")
+        print(serializer.errors["non_field_errors"][0])
+        messages.error(request, serializer.errors["non_field_errors"][0])
+        return render(request, self.template_name)
+    
+class LoginView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = Loginserializer
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = "login.html"
+
+    def get(self, request: Union[Request, HttpRequest]) -> Union[render, redirect]:
+        if "username" in request.session:
+            return redirect("home")
+        return render(request, self.template_name)
+
+    def post(self, request: Union[Request, HttpRequest]) -> Union[render, redirect]:
+        serializer = Loginserializer(data=request.data)
+        if serializer.is_valid():
+            username = serializer.data.get("username")
+            request.session["username"] = username
+            return redirect("home")
+        messages.error(request, serializer.errors["non_field_errors"][0])
+        return render(request, self.template_name)
