e22fbf57fac98ef660821ba0310d2ea78f49fb49
Jay Desai <jaydesai2304@gmail.com>
2024-09-03 11:07:05 +0530

    remove extra part and add contact page

--- drivenow/drive/urls.py
+++ drivenow/drive/urls.py
@@ -11,6 +11,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("index/", views.index, name="index"),
     path("about/", views.about, name="about"),
+    path("contact/", views.contact, name="contact"),
 
     path("", RegisterView.as_view(), name="register"),
     path("login", LoginView.as_view(), name="login"),
