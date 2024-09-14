6e1d89a67181da92bd43f5d5414c784181250a4d
harsh230806 <harsh@nexalussoft.com>
2024-09-14 10:23:41 +0530

    minor changes

--- drivenow/drive/urls.py
+++ drivenow/drive/urls.py
@@ -5,11 +5,13 @@ from .views import (
     LoginView,
     ContactView
 )
+from django.views.generic import RedirectView
 from django.conf import settings
 from . import views
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path("", RedirectView.as_view(pattern_name="index", permanent=False)),
     path("index/", views.index, name="index"),
     path("about/", views.about, name="about"),
     path("cars/", views.cars, name="cars"),
@@ -19,8 +21,8 @@ urlpatterns = [
     path("orders/", views.orders, name="orders"),
     path("favorite/", views.favorite, name="favorite"),
 
-    path("", RegisterView.as_view(), name="register"),
+    path("register", RegisterView.as_view(), name="register"),
     path("login", LoginView.as_view(), name="login"),
     path("contact/", ContactView.as_view(), name="contact"),
-
+    path("logout", views.logout, name="logout")
 ]
